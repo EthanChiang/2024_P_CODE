5ee259f727a0e1733585b272029f63e91f6cf609
EthanChiang <ethan0514@gmail.com>
2024-05-29 11:07:50 +0800

    commit all

--- Python proj1/index.py
+++ Python proj1/index.py
@@ -38,9 +38,65 @@ def home():
     currency = r.json()
     total = math.floor(taiwanese_dollars + us_dollars *
                        currency['USDTWD']['Exrate'])
-    data = {'total': total, 'td': taiwanese_dollars,
-            'ud': us_dollars, 'currency': currency['USDTWD']['Exrate'], 'cash_result': cash_result}
     # print(currency)
+
+    # 取得所有股票資訊
+    result2 = cursor.execute("select * from stock")
+    stock_result = result2.fetchall()
+    # print(stock_result)
+    # 取得台股多筆相同股的資料,例如買入了好幾次台積電,事實上只有一筆資料
+    unique_stock_list = []
+    for item in stock_result:
+        if item[1] not in unique_stock_list:
+            unique_stock_list.append(item[1])
+        # 計算單一股票資訊
+    stock_info = []
+    for stock in unique_stock_list:
+        result = cursor.execute(
+            "select * from stock where stock_id =?", (stock, ))
+        result = result.fetchall()
+        # print(result)
+
+        stock_cost = 0  # 單一股票總花費
+        shares = 0  # 單一股票股數
+        for d in result:
+            shares += d[2]
+            stock_cost += d[2] * d[3] + d[4] + d[5]
+        # 取得目前股價
+        url = "https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&stockNo=" + stock
+        response = requests.get(url)
+        data1 = response.json()
+        # print(data)
+        price_array = data1['data']
+        current_price = float(price_array[len(price_array) - 1][6])
+        # 單一股票總市值
+        total_value = round(current_price * shares)
+        total_stock_value = 0
+        total_stock_value += total_value
+        # 單一股票平均成本
+        average_cost = round(stock_cost / shares, 2)
+        # 單一股票報酬率
+        rate_of_return = round((total_value - stock_cost)
+                               * 100 / stock_cost, 2)
+        stock_info.append({'stock_id': stock, 'stock_cost': stock_cost,
+                           'total_value': total_value, 'average_cost': average_cost,
+                           'shares': shares, 'current_price': current_price, 'rate_of_return': rate_of_return})
+
+    for stock in stock_info:
+        stock['value_percentage'] = round(
+            stock['total_value'] * 100 / total_stock_value, 2)
+
+    # transcation_id = '2'
+    # cursor.execute("""delete from stock where transaction_id=?""",
+    #                (transcation_id,))
+    # result = cursor.execute("select * from stock")
+    # result1 = result.fetchall()
+    # print(result1)
+    # conn.commit()
+    # print(data)
+    data = {'total': total, 'td': taiwanese_dollars,
+            'ud': us_dollars, 'currency': currency['USDTWD']['Exrate'], 'cash_result': cash_result, "stock_info": stock_info}
+
     return render_template("index.html", data=data)
 
 
