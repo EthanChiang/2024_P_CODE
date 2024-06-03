3336264b8b6f353af50544c61bb260d47654fc72
EthanChiang <ethan0514@gmail.com>
2024-06-04 06:20:37 +0800

    commit all

--- leetcode.py
+++ leetcode.py
@@ -1,8 +1,8 @@
-list1 = [(1, 2, 3), (4, 5, 6)]
-result = 0
-for data in list1:
-    print(data)
-    print(type(data))
-    result += data[1]
+# list1 = [(1, 2, 3), (4, 5, 6)]
+# result = 0
+# for data in list1:
+#     print(data)
+#     print(type(data))
+#     result += data[1]
 
-print(data[1])
+# print(data[1])
