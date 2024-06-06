2e24bf78e14433c21a5f5714c955769f2003080e
EthanChiang <ethan0514@gmail.com>
2024-06-06 22:49:13 +0800

    add TicTacToe GAME

--- AI course/proj3/app.js
+++ AI course/proj3/app.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
   $(".headerPage").load("header.html");
   loadJsonData();
+  TacTicTok();
   // dataTable();
 
   // loadDataTable();
@@ -37,21 +38,6 @@ function loadDataToJQData() {
 
 function dataTable(data) {
   // console.log("123:", data);
-  // var data = [
-  //   [1, 2, 3],
-  //   [3, 4, 3],
-  //   [3, 4, 4],
-  //   [3, 4, 4],
-  //   [3, 4, 4],
-  //   [3, 4, 4],
-  //   [3, 4, 4],
-  //   [3, 4, 4],
-  //   [3, 4, 4],
-  //   [3, 4, 4],
-  //   [3, 4, 4],
-  //   [3, 4, 4],
-  //   [3, 4, 4],
-  // ];
   $("#myTable").DataTable({
     data: data,
     ordering: false,
@@ -81,4 +67,87 @@ function loadDataTable() {
       rows += "</table>";
       content.innerHTML = rows;
     });
+}
+
+function TacTicTok() {
+  let boxes = document.querySelectorAll(".box");
+  let resetBtn = document.querySelector("#reset-btn");
+  let newBtn = document.querySelector("#new-btn");
+  let msgContainer = document.querySelector(".msg-container");
+  let msg = document.querySelector("#msg");
+  let turn0 = true; //playerX,player0
+
+  const winPatterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8],
+  ];
+
+  boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+      console.log("box was clicked");
+      if (turn0) {
+        box.innerText = "O";
+        turn0 = false;
+      } else {
+        box.innerText = "X";
+        turn0 = true;
+      }
+      box.disabled = true;
+      checkWinner();
+    });
+  });
+  const showWinner = (winner) => {
+    msg.innerText = `恭喜, Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+  };
+
+  const resetGame = () => {
+    turn0 = true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+  };
+
+  const disableBoxes = () => {
+    for (let box of boxes) {
+      box.disabled = true;
+    }
+  };
+
+  const enableBoxes = () => {
+    for (let box of boxes) {
+      box.disabled = false;
+      box.innerText = "";
+    }
+  };
+
+  const checkWinner = () => {
+    for (let pattern of winPatterns) {
+      // console.log(pattern[0], pattern[1], pattern[2]);
+      // console.log(
+      //   boxes[pattern[0]].innerText,
+      //   boxes[pattern[1]].innerText,
+      //   boxes[pattern[2]].innerText
+      // );
+      let pos1Val = boxes[pattern[0]].innerText;
+      let pos2Val = boxes[pattern[1]].innerText;
+      let pos3Val = boxes[pattern[2]].innerText;
+      if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+        if (pos1Val == pos2Val && pos2Val == pos3Val) {
+          console.log("winner", pos1Val);
+
+          showWinner(pos1Val);
+        }
+      }
+    }
+  };
+
+  newBtn.addEventListener("click", resetGame);
+  resetBtn.addEventListener("click", resetGame);
 }
